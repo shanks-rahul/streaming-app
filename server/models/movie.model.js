a93edda197b886d8678d0ed0104e2898e2dd2823
shanks-rahul <rahulagarwalla1234@gmail.com>
2024-02-27 12:23:41 +0530

    added the database model for movie section

---
+++ server/models/movie.model.js
@@ -0,0 +1,55 @@
+import { Schema, model } from "mongoose";
+import { stringify } from "querystring";
+const movieSchema = Schema.create({
+    title: {
+        type: String,
+        required: [true, "Title is Required"],
+    },
+    description: {
+        type: String,
+        required: [true, 'Description is required'],
+        minlength: [20, 'Description must be atleast 20 characters long'],
+    },
+    category: {
+        type: String,
+        required: [true, 'Category is required'],
+    },
+    episodes: [
+        {
+            title: String,
+            description: String,
+            episode: {
+                public_id: {
+                    type: String,
+                    required: true,
+                },
+                secure_url: {
+                    type: String,
+                    required: true,
+                }
+            }
+        },
+    ],
+    thumbnail: {
+        public_id: {
+            type: String,
+        },
+        secure_url: {
+            type: String,
+        },
+    },
+    numberOfEpisodes: {
+        type: Number,
+    },
+    directedBy:{
+        type:String,
+        required:true,
+    },
+    producedBy:{
+        type:string,
+        required:true
+    }
+});
+
+const Movie=model("Movie",movieSchema);
+export default Movie;
