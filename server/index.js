34ef8b0c2e2c7a92aa335addb9b40f0a7c0ea486
shanks-rahul <rahulagarwalla1234@gmail.com>
2024-03-01 16:21:10 +0530

    added the razorpay configuration

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 import app from "./app.js";
 import { v2 } from "cloudinary";
 import connectToDB from "./db/configs/dbConn.js";
+import Razorpay from 'razorpay';
 
 v2.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+export const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_SECRET,
+  });
+  
 
 const PORT=process.env.PORT;
 app.listen(PORT,async()=>{
