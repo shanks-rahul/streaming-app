7f94fd1a8084cfa315d2b51d4739ead897161789
shanks-rahul <123451654+shanks-rahul@users.noreply.github.com>
2024-05-11 19:45:07 +0530

    Update user.controller.js

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -38,20 +38,19 @@ export const register = asyncHandler(async (req, res, next) => {
     if (req.file) {
         try {
             const result = await cloudinary.v2.uploader.upload(req.file.path, {
-                folder: 'videostream-app', // Save files in a folder named lms
+                folder: 'videostream-app',
                 width: 250,
                 height: 250,
-                gravity: 'faces', // This option tells cloudinary to center the image around detected faces (if any) after cropping or resizing the original image
+                gravity: 'faces',
                 crop: 'fill',
             });
 
-            // If success
             if (result) {
-                // Set the public_id and secure_url in DB
+               
                 user.avatar.public_id = result.public_id;
                 user.avatar.secure_url = result.secure_url;
 
-                // After successful upload remove the file from local storage
+                
                 fs.rm(`uploads/${req.file.filename}`);
             }
         } catch (error) {
